7de6b96b56878802ed78fd84bff20daf65037340
margb13 <marg.b13@hotmail.com>
2017-11-20 11:43:34 -0500

    Version 0.0.2

--- js/app.js
+++ js/app.js
@@ -4,15 +4,62 @@ window.addEventListener("load", function () {
   var saveListBox = document.getElementById('save-list-box');
   var listInputBox = document.getElementById('list-input-box');
   var saveListBtn = document.getElementById('save-list-btn');
+  var textValue;
+  var data = { lists: [] };
 
-
-
-  
   addListBtn.addEventListener("click", function (event) {
     saveListBox.style.display = "inline-block";
     addListBtn.style.display = "none";
   });
+  
+  saveListBtn.addEventListener("click", function (event) {
+    textValue = listInputBox.value;
+    if (textValue) {
+      var parentDiv = document.createElement("div");
+      var listTitle = document.createElement("div");
+      var title = document.createElement("div");
+      var dotIcon = document.createElement("a");
+      var newTextNode = document.createTextNode(textValue);
+      var addCardLink = document.createElement("a");
+      var addText = document.createTextNode("Añadir una tarea...");
+      var lists;
+      
+      data.lists.push( {title: textValue, cards: []} );
+      lists = data.lists.map(function (event) {
+        return event;
+      });
+      for (var i = 0; i < lists.length; i++) {
+        parentDiv.setAttribute("class", "add-card-container");
+        parentDiv.setAttribute("id", "list-container" + i);
+        dotIcon.setAttribute("class", "fa fa-ellipsis-h circle");
+        dotIcon.setAttribute("id", "del" + i);
+        title.setAttribute("contenteditable", "true");
+        title.setAttribute("class", "text");
+        title.appendChild(newTextNode);
+        listTitle.appendChild(dotIcon);
+        listTitle.setAttribute("class", "list-title");
+        listTitle.appendChild(title);
+        addCardLink.setAttribute("href", "#");
+        addCardLink.setAttribute("class", "add-card");
+        addCardLink.setAttribute("id", "add" + i);
+        addCardLink.appendChild(addText);
+        parentDiv.style.cssFloat = "left";
+        parentDiv.style.display = "inline-block";
+        parentDiv.appendChild(listTitle);
+        parentDiv.appendChild(addCardLink);
+        container.insertBefore(parentDiv, addListBtn);
+        addListBtn.style.display = "inline-block";
+        saveListBox.style.display = "none";
+        listInputBox.value = "";
+        
+        }
+    }
+    
+    
+    
+    });
+
+
 
 });
-
 
